35e7b1f65c734b03c491c471582622767d103e8d
kadiirhocaoglu <kadiirhocaoglu@hotmail.com>
2024-07-21 17:31:30 +0300

    21 july animations container, text, button and cross fade

--- animated_app/lib/animated_learn_view.dart
+++ animated_app/lib/animated_learn_view.dart
@@ -7,8 +7,17 @@ class AnimatedLearnView extends StatefulWidget {
   State<AnimatedLearnView> createState() => _AnimatedLearnViewState();
 }
 
-class _AnimatedLearnViewState extends State<AnimatedLearnView> {
-  bool isVisible = true;
+class _AnimatedLearnViewState extends State<AnimatedLearnView>  with TickerProviderStateMixin {
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(vsync: this, duration: Duration(seconds: 2));
+  }
+    bool isVisible = true;
+  bool isOpacity = true;
+  late AnimationController controller;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,15 +25,32 @@ class _AnimatedLearnViewState extends State<AnimatedLearnView> {
         IconButton(onPressed: (){
           setState(() {
             isVisible = !isVisible;
+            controller.animateTo( isVisible ? 1:0);
           });
         }, icon: Icon(Icons.change_circle))
       ],),
-      body: AnimatedCrossFade(
-      firstChild: Container(decoration: BoxDecoration(color: Colors.red),),
-      secondChild: Container(decoration: BoxDecoration(color: Colors.green),),
-      crossFadeState: isVisible ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-      duration: Duration(seconds: 1),
-    ),
+      body: ListView(
+        children: [
+          Container(
+            height: 100,
+            child: AnimatedCrossFade(
+            firstChild: Container(decoration: BoxDecoration(color: Colors.red),),
+            secondChild: Container(decoration: BoxDecoration(color: Colors.green),),
+            crossFadeState: isVisible ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+            duration: Duration(seconds: 1),
+                ),
+          ),
+          Container(child: ListTile(title: AnimatedOpacity(opacity: isVisible ? 1 : 0, duration: Duration(seconds: 1), child: Text("Merhaba")))),
+          Container(
+            child: AnimatedDefaultTextStyle(child: const Text("Data"), style: (isVisible ? Theme.of(context).textTheme.headlineLarge : Theme.of(context).textTheme.displayLarge) ?? TextStyle(), duration: Duration(seconds: 2),),
+          ),
+          Container(height: 540, decoration: BoxDecoration(color: Colors.amber),),
+          Container(
+            child: AnimatedIcon(progress: controller, icon: AnimatedIcons.arrow_menu),
+          ),
+          AnimatedContainer(duration: Duration(seconds: 2), height: isVisible ? 100 : 0, decoration: BoxDecoration(color: Colors.blue),)
+        ],
+      ),
     );
   }
 }
