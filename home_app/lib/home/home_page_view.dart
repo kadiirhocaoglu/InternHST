e62a560b493799c2ce67117fb67225497d291516
kadiirhocaoglu <kadiirhocaoglu@hotmail.com>
2024-07-22 11:32:55 +0300

    created home page to navigate bar

--- home_app/lib/home/home_page_view.dart
+++ home_app/lib/home/home_page_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home_app/settings/settings_view.dart';
 
 class HomePageView extends StatefulWidget {
   const HomePageView({super.key});
@@ -10,13 +11,38 @@ class HomePageView extends StatefulWidget {
 class _HomePageViewState extends State<HomePageView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(leading: Icon(Icons.menu),
-        title: Text("HST POS"),
-        actions: [Icon(Icons.notifications)],
-      ),
-      body: ,
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
 
+    return Scaffold(
+        body: Column(
+          children: [
+            Container(
+              height: height / 3,
+              width: width,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                const Color.fromARGB(255, 41, 130, 44),
+                Color.fromARGB(255, 14, 42, 135)
+              ])),
+              child: Column(
+                children: [],
+              ),
+            ),
+            Expanded(
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: ListView(
+                    children: [
+                      SizedBox(child: Text("data",), height: 100, width: width,),
+                      SizedBox(child: Text("data"), height: 100, width: width,),
+                      SizedBox(child: Text("data"), height: 100, width: width,),
+                  ],
+                              ),
+                )),
+                
+          ],
+        ),
     );
   }
 }
